9e40dd721e0aa3aad647600d08141a0dbd5e0d52
MohamedMaarka <120613022+MohamedMaakra@users.noreply.github.com>
2023-06-14 14:00:34 +0200

    mise en place du conteur sur le carrousel

--- my-app/src/components/Banniere.jsx
+++ my-app/src/components/Banniere.jsx
@@ -1,28 +1,36 @@
 import React, { useState } from 'react';
-import '../Styles/Banniere.css'
+import '../Styles/Banniere.css';
 import prev from '../assets/prev.png';
-import next from '../assets/next.png'
+import next from '../assets/next.png';
 
 const Carousel = ({ pictures }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const totalPictures = pictures.length;
 
   const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === pictures.length - 1 ? 0 : prevIndex + 1));
+    setCurrentIndex((prevIndex) => (prevIndex === totalPictures - 1 ? 0 : prevIndex + 1));
   };
 
   const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? pictures.length - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? totalPictures - 1 : prevIndex - 1));
   };
 
   return (
     <div className="carousel">
-      <button className="carousel-button fleche-prev" onClick={prevSlide}>
-      <img className='fleche' src={prev} alt="prev"  />
-      </button>
+      {totalPictures > 1 && (
+        <button className="carousel-button fleche-prev" onClick={prevSlide}>
+          <img className='fleche' src={prev} alt="prev"  />
+        </button>
+      )}
       <img src={pictures[currentIndex]} alt="Carousel Slide" className="carousel-image" />
-      <button className="carousel-button fleche-next" onClick={nextSlide}>
-       <img className='fleche' src={next} alt="next"  />
-      </button>
+      {totalPictures > 1 && (
+        <button className="carousel-button fleche-next" onClick={nextSlide}>
+          <img className='fleche' src={next} alt="next"  />
+        </button>
+      )}
+      {totalPictures > 1 && (
+        <div className="carousel-counter">{currentIndex + 1}/{totalPictures}</div>
+      )}
     </div>
   );
 };
